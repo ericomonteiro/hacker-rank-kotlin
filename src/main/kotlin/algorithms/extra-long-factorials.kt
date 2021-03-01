9079e73ee472c5a9f486ca75d8eb579d2a5c1923
Erico Monteiro <erico.cintra@gmail.com>
2021-03-01 20:28:55 -0300

    extra long factorials

---
+++ src/main/kotlin/algorithms/extra-long-factorials.kt
@@ -0,0 +1,24 @@
+package algorithms
+
+import java.math.BigDecimal
+import java.util.Scanner
+
+//https://www.hackerrank.com/challenges/extra-long-factorials/problem
+
+fun extraLongFactorials(n: Int): Unit {
+    var result = n.toBigDecimal()
+    for (i in n downTo 2) {
+        result *= BigDecimal(i - 1)
+    }
+
+    println(result)
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+//
+//    val n = scan.nextLine().trim().toInt()
+
+    val n = 25
+    extraLongFactorials(n)
+}
