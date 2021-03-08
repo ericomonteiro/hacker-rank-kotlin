2de5277ade0b56d554171a6d9711fb6f1d4bdda8
Erico Monteiro <erico.cintra@gmail.com>
2021-03-08 01:17:33 -0300

    jumping-on-the-clouds

---
+++ src/main/kotlin/algorithms/jumping-on-the-clouds.kt
@@ -0,0 +1,50 @@
+package algorithms
+
+import java.util.Scanner
+
+//https://www.hackerrank.com/challenges/jumping-on-the-clouds/submissions/code/201932703
+
+/**
+ * Input (stdin)
+ * 7
+ * 0 0 1 0 0 1 0
+ *
+ * Expected Output
+ * 4
+ */
+
+// Complete the jumpingOnClouds function below.
+fun jumpingOnClouds(c: Array<Int>): Int {
+    var jumps = 0
+
+    var i = 0
+    while ( i < c.size-1) {
+        if (c.size < i+3) {
+            jumps++
+            break
+        }
+
+        if (c[i+2] == 0) {
+            i += 2
+        } else {
+            i ++
+        }
+        jumps++
+    }
+
+    return jumps
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val n = scan.nextLine().trim().toInt()
+
+    val c = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+
+    val result = jumpingOnClouds(c)
+
+    println(result)
+}
+
+
