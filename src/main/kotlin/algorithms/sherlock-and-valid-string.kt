cd880ee8b3b343bb0f2efee9da4364e8872f2f1e
Erico Monteiro <erico.cintra@gmail.com>
2021-03-08 01:20:16 -0300

    sherlock-and-valid-string

---
+++ src/main/kotlin/algorithms/sherlock-and-valid-string.kt
@@ -0,0 +1,35 @@
+package algorithms
+
+import java.util.Scanner
+
+//https://www.hackerrank.com/challenges/sherlock-and-valid-string/problem
+
+/**
+ * Input (stdin)
+ * aabbccddeefghi
+ *
+ * Expected Output
+ * NO
+ * */
+
+// Complete the isValid function below.
+fun isValid(s: String): String {
+    val map = s.toList().groupingBy { it }.eachCount()
+    val counter = map.values.groupingBy { it }.eachCount()
+
+    if (counter.keys.size == 2 && counter.values.toIntArray()[1] == 1)
+        return "YES"
+    if (counter.keys.size > 1)
+        return "NO"
+    return "YES"
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val s = scan.nextLine()
+
+    val result = isValid(s)
+
+    println(result)
+}
