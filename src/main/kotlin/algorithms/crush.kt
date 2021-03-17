2e2b9b442262f9bf66b30b6cda2cce80b4d125fa
Erico Monteiro <erico.cintra@gmail.com>
2021-03-17 01:39:53 -0300

    crush

---
+++ src/main/kotlin/algorithms/crush.kt
@@ -0,0 +1,63 @@
+package algorithms
+
+import java.util.Scanner
+
+//https://www.hackerrank.com/challenges/crush/problem
+
+// Complete the arrayManipulation function below.
+fun arrayManipulation(n: Int, queries: Array<Array<Int>>): Long {
+    val arrayResult = mutableListOf<Long>()
+
+    var max = 0L
+    queries.forEach {
+        for (i in it[0] - 1 until minOf(it[1], arrayResult.size)) {
+            arrayResult[i] += it[2].toLong()
+            if (max < arrayResult[i]) max = arrayResult[i]
+        }
+
+        for (i in arrayResult.size until it[1]) {
+            if (i >= it[0] - 1) {
+                arrayResult.add(it[2].toLong())
+                if (max < it[2]) max = it[2].toLong()
+            } else {
+                arrayResult.add(0)
+            }
+        }
+    }
+
+    return max
+}
+
+fun main(args: Array<String>) {
+//    val scan = Scanner(System.`in`)
+//
+//    val nm = scan.nextLine().split(" ")
+//
+//    val n = nm[0].trim().toInt()
+//
+//    val m = nm[1].trim().toInt()
+//
+//    val queries = Array<Array<Int>>(m, { Array<Int>(3, { 0 }) })
+//
+//    for (i in 0 until m) {
+//        queries[i] = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+//    }
+
+//    val queries = arrayOf(
+//        arrayOf(1,5,3),
+//        arrayOf(4,8,7),
+//        arrayOf(6,9,1)
+//    )
+
+    val queries = arrayOf(
+        arrayOf(2,3,603),
+        arrayOf(1,1,286),
+        arrayOf(4,4,882)
+    )
+
+    val n = 3
+
+    val result = arrayManipulation(n, queries)
+
+    println(result)
+}
