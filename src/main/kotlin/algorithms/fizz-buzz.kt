e941d8274e7d4dbe23ca79db0d1920c3762fbaa6
ericomonteiro <HKKEpVOVkK060991>
2022-08-03 01:25:18 -0300

    fizz-buzz

---
+++ src/main/kotlin/algorithms/fizz-buzz.kt
@@ -0,0 +1,28 @@
+package algorithms
+
+fun Int.isMultipleOf(n: Int) = this % n == 0
+
+
+
+fun fizzBuzz(n: Int): Unit {
+
+    var byThree: Boolean = false
+    var byFive: Boolean = false
+
+    for (i in 1..n) {
+        byThree = i.isMultipleOf(3)
+        byFive = i.isMultipleOf(5)
+
+        if (byThree) print("Fizz")
+        if (byFive) print("Buzz")
+        if (!byThree && !byFive) print(i)
+        println()
+    }
+
+}
+
+fun main(args: Array<String>) {
+//    val n = readLine()!!.trim().toInt()
+
+    fizzBuzz(15)
+}
